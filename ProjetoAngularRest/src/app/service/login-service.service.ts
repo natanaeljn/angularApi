f9dfe44704498e62ae2f4864dcbeb780cc67a500
natanaeljn <natanaeljn@outlook.com.br>
2023-04-18 12:08:45 -0300

    primeiro commite

---
+++ ProjetoAngularRest/src/app/service/login-service.service.ts
@@ -0,0 +1,35 @@
+
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AppConstants } from '../app-constants';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginServiceService {
+
+  constructor(private http: HttpClient) { }
+  
+ login(usuario){
+       
+       return this.http.post(AppConstants.baseLogin ,JSON.stringify(usuario)).subscribe(data => {
+
+          /*Retorno Http*/ 
+
+          var token = JSON.parse(JSON.stringify(data)).Authorization.split(' ')[1];
+
+          localStorage.setItem("token", token);
+
+          console.info("Tohken: " + localStorage.getItem("token"));
+
+
+       },
+         error => {
+      
+          console.error("Erro ao fazer login ");
+          alert('Acesso Negado!')
+         }
+       );
+    }
+
+}
